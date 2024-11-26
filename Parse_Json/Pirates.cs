572f6f8f2312ab0d0afe95907257ddd4ed414263
anantsingh <anant.singh@mismosystems.com>
2024-11-26 08:54:53 +0530

    second commit.

---
+++ Parse_Json/Pirates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parse_Json
+{
+    public class Pirates
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public long bounty { get; set; }
+        public string crew { get; set; }
+    }
+}
